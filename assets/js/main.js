f8bb1b5e313c51ac8955b8e1e89a55f8522558d6
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-28 14:34:46 +0200

    added object todo list

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,20 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 createApp({
     data() {
         return {
-
+            tasks: [
+                {
+                    text: "Learn HTML",
+                    done: true
+                },
+                {
+                    text: "Learn CSS",
+                    done: false
+                },
+                {
+                    text: "Learn JS",
+                    done: false
+                }
+            ]
         }
     }
 }).mount('#app');
