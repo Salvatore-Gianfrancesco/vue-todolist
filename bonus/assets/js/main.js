8e0967f6156e79ed0ed67e9413c2753c7780c9d2
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-28 15:53:52 +0200

    added @keydown.enter event on input

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,45 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            newTask: "",
+            error: false,
+            tasks: [
+                {
+                    text: "Learn HTML",
+                    done: true
+                },
+                {
+                    text: "Learn CSS",
+                    done: false
+                },
+                {
+                    text: "Learn JS",
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        cancelTask(i) {
+            // console.log(i);
+            this.tasks.splice(i, 1);
+        },
+        addTask(taskText) {
+            // console.log(this.newTask);
+            // console.log(this.newTask.length);
+
+            if (taskText.length < 5) {
+                this.error = true;
+            } else {
+                this.tasks.unshift({
+                    text: taskText,
+                    done: false
+                });
+                this.newTask = "";
+                this.error = false;
+            }
+        }
+    }
+}).mount('#app');
